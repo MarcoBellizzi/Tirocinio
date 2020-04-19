fdacfac6a04ddeef5bc57cab5acc744dfe120d4b
MarcoBellizzi <marcobellizzi96@gmail.com>
2020-04-19 11:40:14 +0200

    updated shortest path python

---
+++ ShortestPathPython/src/shortest_path/main.py
@@ -0,0 +1,97 @@
+from shortest_path.Edge import Edge
+from shortest_path.Path import Path
+from platforms.desktop.desktop_handler import DesktopHandler
+from specializations.dlv2.desktop.dlv2_desktop_service import DLV2DesktopService
+from languages.asp.asp_mapper import ASPMapper
+from languages.asp.asp_input_program import ASPInputProgram
+
+
+def getEdges():
+    edges = []
+    
+    edges.append(Edge(0,1,1))
+    edges.append(Edge(0,2,4))
+    edges.append(Edge(1,2,2))
+    edges.append(Edge(1,3,4))
+    edges.append(Edge(1,4,1))
+    edges.append(Edge(2,4,4))
+    edges.append(Edge(3,5,6))
+    edges.append(Edge(3,6,1))
+    edges.append(Edge(4,3,1))
+    edges.append(Edge(6,4,5))
+    edges.append(Edge(6,5,9))
+    edges.append(Edge(6,7,1))
+    edges.append(Edge(7,5,2))
+    
+    return edges
+
+def join(source, path, sortedPath):
+    for p in path :
+        if(int(p.get_source()) == int(source)) :
+            sortedPath.append(p.get_destination())
+            if(int(p.get_destination()) == destination) :
+                return
+            join(p.get_destination(), path, sortedPath)
+            return
+
+def show(path, sum_):
+    first = True
+    print("path = ", end='')
+    for n in path :
+        if not first :
+            print(" - ", end='')
+        else :
+            first = False
+        print(n, end='')
+    print("\nsum = " + str(sum_))
+    
+
+try:
+    
+    handler = DesktopHandler(DLV2DesktopService("../../lib/dlv2"))
+
+    ASPMapper.get_instance().register_class(Edge)
+    ASPMapper.get_instance().register_class(Path)
+
+    inputProgram = ASPInputProgram()
+
+    source = 0
+    destination = 7
+    
+    rules = "path(X,Y,W) | notPath(X,Y,W) :- source(X), edge(X,Y,W)."
+    rules += "path(X,Y,W) | notPath(X,Y,W) :- path(_,X,_), edge(X,Y,W)."
+    rules += "end(X) :- destination(X), path(_,X,_)."
+    rules += ":- destination(X), not end(X)."
+    rules += ":~ path(X,Y,W). [W@1 ,X,Y]"
+
+    inputProgram.add_program(rules)
+    inputProgram.add_program("source(" + str(source) + "). destination(" + str(destination) + ").")
+    
+    inputProgram.add_objects_input(getEdges())
+
+    handler.add_program(inputProgram)
+    
+    answerSets = handler.start_sync()
+
+    for answerSet in answerSets.get_optimal_answer_sets():
+    
+        path = []
+        sum_ = 0
+    
+        for obj in answerSet.get_atoms():
+        
+            if isinstance(obj, Path):
+           
+                path.append(obj)
+                sum_ += int(obj.get_weight())
+
+        sortedPath = []
+        sortedPath.append(source)
+    
+        join(source, path, sortedPath)
+        show(sortedPath, sum_)
+    
+except Exception as e:
+    print(str(e))
+    
+    
